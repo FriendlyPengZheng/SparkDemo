dea9061cf7a7963080415b06e02337563d96e1bf
FriendlyPengZheng <18627239055@163.com>
2019-08-27 18:51:20 +0800

    partiton test

--- src/main/java/rddDemo/RDDPartitionTest.java
+++ src/main/java/rddDemo/RDDPartitionTest.java
@@ -1,10 +1,30 @@
 package rddDemo;
 
+import java.util.Arrays;
+
+import org.apache.spark.Partitioner;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.Optional;
+
 public class RDDPartitionTest {
 
 	public static void main(String[] args) {
 		
+		SparkConf conf = new SparkConf()
+		.setMaster("local")
+		.setAppName("pairRDD test");
+
+		JavaSparkContext sc = new JavaSparkContext(conf);
 		
+		JavaRDD<Integer> input = sc.parallelize(Arrays.asList(1,2,3,4,5));
+		
+		Optional<Partitioner> parts = input.partitioner();
+		
+		System.out.println(parts);
+		
+		sc.close();
 	}
 
 }
